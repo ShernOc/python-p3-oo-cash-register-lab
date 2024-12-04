07a860e289da9bf25764fdaaeb37680953889d66
ShernOC <sherlynea1994@hotmail.com>
2024-12-04 20:17:04 +0300

    functions

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,66 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  
+  def __init__(self,total,discount):
+    self.total = total
+    self.discount= discount
+  
+  def register_total(self):
+    pass 
+  
+  def discount_attribute(self):
+    pass 
+  
+  def total_attribute(self):
+    pass 
+  
+  def items_attribute(self):
+    pass 
+  
+  def add_item(self):
+    pass 
+  
+  def add_item_optional(self):
+    pass 
+  
+  def add_item_with_multiple_items(self):
+    pass 
+  
+  def apply_discount(self):
+     pass 
+   
+  
+  def apply_discount_success_message(self):
+    pass 
+  
+
+  def apply_discount_reduces_total(self):
+    pass 
+  
+
+
+  def apply_discount_when_no_discount(self): 
+    pass 
+  
+  
+  def list_without_multiples(self):
+    pass 
+  
+  def list_with_multiples(self):
+   pass
+ 
+ 
+def last_transaction(self):
+  
+  pass 
+  
+def last_transaction_with_multiples(self):
+  pass 
+    
+    
+    
+
+
+
+
